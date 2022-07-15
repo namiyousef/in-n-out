7d5015163f238c9a8045678af30ba702b90101b8
Yousef <namiyousef@hotmail.com>
2022-07-15 23:54:24 +0100

    adds basic postgres client

---
+++ in_n_out/client.py
@@ -0,0 +1,17 @@
+import sqlalchemy as db
+
+class PostgresClient:
+    def __init__(self, username, password, host, port, name):
+        self.db_user = username
+        self.db_password = password
+        self.db_host = host
+        self.db_port = port
+        self.db_name = name
+        self.db_uri = f'postgresql+psycopg2://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}'
+
+    def initialise_client(self):
+        self.engine = db.create_engine(self.db_uri)
+        self.con = self.engine.connect()
+
+    def query(self):
+        pass
