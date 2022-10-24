9f67ee3fd248ab1561a86dfc0a878c8915cccf5e
Yousef <namiyousef@hotmail.com>
2022-10-24 16:04:11 +0100

    removes iteritems as it is being deprecated

--- in_n_out/utils.py
+++ in_n_out/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pandas.api.types import is_datetime64tz_dtype
 
 def _convert_timezone_columns_to_utc(df):
-    for col, dtype in df.dtypes.iteritems():
+    for col, dtype in df.dtypes.items():
         if is_datetime64tz_dtype(dtype):
             df[col] = pd.to_datetime(df[col], utc=True)
 
