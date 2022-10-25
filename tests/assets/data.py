75364af9f3eda2543b9908d27afbe7e9284e8b34
Yousef <namiyousef@hotmail.com>
2022-10-25 20:50:13 +0100

    updates test data assets

--- tests/assets/data.py
+++ tests/assets/data.py
@@ -2,36 +2,37 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import pytz
+# TODO this import needs to come from somewhere else
+from in_n_out.utils import _convert_timezone_columns_to_utc
+from functools import partial
 
+_gen_date = partial(dt.datetime, 1999, 12, 31)
+MILLENNIUM_END = _gen_date()
+MILLENNIUM_END_UTC = _gen_date(tzinfo=pytz.timezone('UTC'))
+MILLENNIUM_END_PACIFIC = _gen_date(tzinfo=pytz.timezone('US/Pacific'))
+MILLENNIUM_END_OFFSET = _gen_date(tzinfo=pytz.FixedOffset(1))
 DF_INITIAL = pd.DataFrame({
-    'datetime': [dt.datetime.now()],
-    'datetime_utc': [dt.datetime.now(tz=pytz.timezone('UTC'))],
-    'datetime_pacific': [dt.datetime.now(tz=pytz.timezone('US/Pacific'))],
+    'datetime': [MILLENNIUM_END],
+    'datetime_utc': [MILLENNIUM_END_UTC],
+    'datetime_pacific': [MILLENNIUM_END_PACIFIC],
+    'datetime_offset': [MILLENNIUM_END_OFFSET],
     'float': [1.0],
     'string': ['test_string'],
     'int': [1],
     'boolean': [True],
 })
-
-DF_NULLS = pd.DataFrame({
-    'datetime': [np.datetime64('NaT'), None, np.nan],
-    #'float': [],
-    #'string': [],
-    #'boolean': []
+# TODO also missing coverage is having NaT on datetime with offsets?
+DF_MIXED = pd.DataFrame({
+    'datetime': [MILLENNIUM_END_OFFSET, None],
+    'datetime_utc': [MILLENNIUM_END, np.datetime64('NaT')],
+    'datetime_pacific': [MILLENNIUM_END_PACIFIC, np.nan],
+    # missing final datetime (offset)
+    'float': [np.nan, None],
+    'string': ['', None],
+    'int': [2, 3], # TODO missing test coverage: int that gets corrupted into float by nan
+    'boolean': [False, None]
 })
 
-if __name__ == '__main__':
-    DF_INITIAL.info()
-
-    DF_NULLS.info()
-    '''save_type = 'parquet'
-    stress = False
-    name = f'df_initial.{save_type}' if not stress else f'df_stress.{save_type}'
-    df_save = DF_INITIAL if not stress else pd.concat([DF_INITIAL]*50000)
-    if save_type == 'parquet':
-        df_save.to_parquet(name, engine='pyarrow')'''
-
-    df_1 = pd.DataFrame({'test': [dt.datetime.now(tz=pytz.timezone('UTC'))]})
-    df_2 = pd.DataFrame({'test': [dt.datetime.now(tz=pytz.timezone('US/Pacific'))]})
-    df_1.info()
-    df_2.info()
+# TODO bug! if you pass a date to date+tz then pandas will not cause an error!
+DF_INITIAL_LOCALIZED = _convert_timezone_columns_to_utc(DF_INITIAL.copy())
+DF_MIXED_LOCALIZED = _convert_timezone_columns_to_utc(DF_MIXED.copy())
