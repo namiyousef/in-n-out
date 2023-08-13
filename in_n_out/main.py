10dd2b1e635ed5e7f11806b6d79878ccbec093d5
Yousef <namiyousef@hotmail.com>
2023-08-13 14:57:32 +0200

    #7 adds interface to work with InNOutClient

--- in_n_out/main.py
+++ in_n_out/main.py
@@ -1,17 +1,41 @@
 import io
+import json
 import logging
+from functools import partial
 from typing import List
 
 import pandas as pd
 import sqlalchemy as db
 from fastapi import FastAPI, File, Response, UploadFile
 from fastapi.responses import StreamingResponse
+from in_n_out_clients import InNOutClient
 from in_n_out_clients.email_client import EmailClient
-from in_n_out_clients.google_calendar_client import GoogleCalendarClient
+from in_n_out_clients.in_n_out_types import ConflictResolutionStrategy
 from in_n_out_clients.postgres_client import PostgresClient
 from pydantic import BaseModel, Json
 
 app = FastAPI()
+
+
+def get_file_type(file):
+    if "json" in file.content_type:
+        return "json"
+    elif "csv" in file.content_type:
+        return "csv"
+    elif file.content_type == "text/tab-separated-values":
+        return "tsv"
+    elif file.content_type == "application/octer-stream":
+        return "parquet"
+
+
+FILE_TYPE_TO_PANDAS_PARSER_MAPPING = {
+    "json": pd.read_json,
+    "csv": pd.read_csv,
+    "tsv": partial(pd.read_csv, delimeter="\t"),
+    "parquet": partial(pd.read_parquet, engine="pyarrow"),
+}
+
+FILE_TYPE_PARSER_MAPPING = {"json": json.load}
 
 
 """with open('../logging_config.yaml') as f:
@@ -51,16 +75,18 @@ class IngestionParams(BaseModel):
 
 
 class InsertionParams(BaseModel):
-    database_name: str
     table_name: str
     database_type: str
+    database_name: str | None = None
 
-    conflict_resolution_strategy: str = "replace"  # make this an enum!
-    username: str | None = "username"
-    password: str | None = "password"
-    port: int | None = 5432
-    host: str | None = "localhost"
-    dataset_name: str | None = "dataset"
+    on_data_conflict: ConflictResolutionStrategy = "fail"
+    on_asset_conflict: ConflictResolutionStrategy = "append"
+    data_conflict_properties: List[str] | None = None
+    username: str | None = None
+    password: str | None = None
+    port: int | None = None
+    host: str | None = None
+    dataset_name: str | None = None
 
 
 # for calendar, database_type is to indicate which calendar resource, e.g.
@@ -163,8 +189,11 @@ async def insert(
     insertion_params: Json[InsertionParams],
     file: UploadFile = File(...),
     limit: int = -1,
+    read_data_as_dataframe: bool = True,
 ):
+    # -- read inputs to api
     insertion_params = insertion_params.dict()
+    logger.info("Loading input file...")
     content = await file.read()
 
     DB_USER = insertion_params["username"]
@@ -174,9 +203,10 @@ async def insert(
     DB_NAME = insertion_params["database_name"]
     DB_TYPE = insertion_params["database_type"]
     table_name = insertion_params["table_name"]
-    conflict_resolution_strategy = insertion_params[
-        "conflict_resolution_strategy"
-    ]
+    on_data_conflict = insertion_params["on_data_conflict"]
+    on_asset_conflict = insertion_params["on_asset_conflict"]
+    data_conflict_properties = insertion_params["data_conflict_properties"]
+
     dataset_name = insertion_params.get("dataset_name", None)
     schema = insertion_params.get(
         "schema", {}
@@ -186,42 +216,91 @@ async def insert(
     print(schema, dataset_name)
 
     # TODO need to clean this up!
+
+    # -- parse input data
+    logger.info("Parsing data...")
     with io.BytesIO(content) as data:
-        if "json" in file.content_type:
-            df = pd.read_json(data)
-        if "csv" in file.content_type:
-            df = pd.read_csv(data)
-        if file.content_type == "text/tab-separated-values":
-            df = pd.read_csv(data, delimiter="\t")
-        if (
-            file.content_type == "application/octet-stream"
-        ):  # TODO can you have other 'octet-stream'?
-            df = pd.read_parquet(data, engine="pyarrow")
+        file_type = get_file_type(file)
+        logger.info(f"Got file_type `{file_type}`")
+        if read_data_as_dataframe:
+            file_reader = FILE_TYPE_TO_PANDAS_PARSER_MAPPING.get(file_type)
+        else:
+            file_reader = FILE_TYPE_PARSER_MAPPING.get(file_type)
 
-    if DB_TYPE == "google_calendar":
-        client = GoogleCalendarClient()
-        client.create_events(
-            df.to_dict(orient="records"),
-            calendar=DB_NAME,
-            conflict_resolution_strategy=conflict_resolution_strategy,
-            create_calendar=False,
-        )
-    elif DB_TYPE == "pg":
-        client = PostgresClient(
-            DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
-        )
+        if file_reader is None:
+            response.status_code = 400
+            _msg = (
+                f"There is no parser for file_type={file_type} and "
+                f"read_data_as_dataframe={read_data_as_dataframe}"
+            )
+            return {"status_code": 400, "msg": _msg}
 
-        client.write(
-            df,
-            table_name,
+        data = file_reader(data)
+
+    # -- connect to client
+    logger.info(f"Connecting to `{DB_TYPE}` client...")
+    try:
+        client = InNOutClient(
+            database_type=DB_TYPE,
+            database_name=DB_NAME,
+            password=DB_PASSWORD,
+            username=DB_USER,
+            host=DB_HOST,
+            port=DB_PORT,
         )
-    else:
+    except ConnectionError as connection_error:
+        _msg = (
+            f"There was a problem connecting to the `{DB_TYPE}` client. "
+            f"Reason: {connection_error}"
+        )
         response.status_code = 400
-        return f"database_type={DB_TYPE} has no support"
-    response.status_code = 201  # TODO technically not correct because we don't
-    # always created the asset, sometimes it is simply a 200 response!
+        logger.error(_msg)
+        return {
+            "status_code": 400,
+            "msg": _msg,
+        }
+    except NotImplementedError as not_implemented_error:
+        _msg = (
+            f"The `{DB_TYPE}` client is not implemented. "
+            f"Reason: {not_implemented_error}"
+        )
+        response.status_code = 501
+        logger.error(_msg)
+        return {
+            "status_code": 501,
+            "msg": _msg,
+        }
+    # TODO -- preprocess data, using mix-n-match
+    # TODO -- maybe read data, e.g. if ingestion parameteres provided...
+    # Not sure if I want to allow this though!
 
-    return "Created table"
+    # -- write data
+    try:
+        resp = client.write(
+            table_name=table_name,
+            data=data,
+            dataset_name=dataset_name,
+            on_data_conflict=on_data_conflict,
+            on_asset_conflict=on_asset_conflict,
+            data_conflict_properties=data_conflict_properties,
+        )
+    except NotImplementedError as not_implemented_error:
+        _msg = (
+            "Got inputs requesting an operation that has not been "
+            f"implemented. Details: {not_implemented_error}"
+        )
+        response.status_code = 501
+        logger.error(_msg)
+        return {"status_code": 501, "msg": _msg}
+    # TODO for each client, need to add a data vlidation errro and return that!
+    except Exception as exception:
+        _msg = f"There was an error in processing the request. Reason: {exception}"
+        response.status_code = 500
+        logger.error(_msg)
+        return {"status_code": 500, "msg": _msg}
+
+    response.status_code = resp["status_code"]
+    return resp
 
 
 @app.delete("/delete")
